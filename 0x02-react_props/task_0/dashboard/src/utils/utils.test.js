2e38458add048eac7decdb5b3812dc9a26c59f54
Anitah Aturinda <104755087+anitahx@users.noreply.github.com>
2023-05-07 13:32:06 +0300

    task 0

---
+++ 0x02-react_props/task_0/dashboard/src/utils/utils.test.js
@@ -0,0 +1,16 @@
+import { getFooterCopy, getFullYear, getLatestNotification } from './utils';
+
+test('returns correct year', () => {
+    expect(getFullYear()).toBe(2023);
+});
+
+test('returns correct footer copy', () => {
+    expect(getFooterCopy(true)).toBe('Holberton School')
+    expect(getFooterCopy(false)).toBe('Holberton School main dashboard')
+});
+
+test('returns correct notifications', () => {
+    expect(getLatestNotification()).toBe(
+        '<strong>Urgent requirement</strong> - complete by EOD'
+    );
+});
