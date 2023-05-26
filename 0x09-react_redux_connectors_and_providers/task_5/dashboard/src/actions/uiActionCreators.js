3f561bc89159a32fe9e4b5512870b2c0f7f725ea
Anitah Aturinda <104755087+anitahx@users.noreply.github.com>
2023-05-26 12:19:35 +0300

    task 5

---
+++ 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/actions/uiActionCreators.js
@@ -0,0 +1,69 @@
+import {
+  LOGIN,
+  LOGOUT,
+  DISPLAY_NOTIFICATION_DRAWER,
+  HIDE_NOTIFICATION_DRAWER,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+} from "./uiActionTypes";
+// import 'node-fetch';
+
+export const login = (email, password) => {
+  return {
+    type: LOGIN,
+    user: { email, password },
+  };
+};
+export const boundLogin = (email, password) => dispatch(login(email, password));
+
+export const logout = () => ({ type: LOGOUT });
+export const boundLogout = () => dispatch(logout());
+
+export const displayNotificationDrawer = () => ({
+  type: DISPLAY_NOTIFICATION_DRAWER,
+});
+export const boundDisplayNotificationDrawer = () => dispatch(displayNotificationDrawer());
+
+export const hideNotificationDrawer = () => ({
+  type: HIDE_NOTIFICATION_DRAWER,
+});
+export const boundHideNotificationDrawer = () => dispatch(hideNotificationDrawer());
+
+export function loginSuccess() {
+	return {
+		type: LOGIN_SUCCESS,
+	};
+}
+
+export function loginFailure() {
+	return {
+		type: LOGIN_FAILURE,
+	};
+}
+
+// async function
+export function loginRequest(email, password) {
+	return async (dispatch) => {
+		// boundLogin(email, password);
+    dispatch(login(email, password));
+
+		try {
+      const res = await fetch('http://localhost:8564/login-success.json');
+      const json = await res.json();
+      return dispatch(loginSuccess());
+    } catch (error) {
+      return dispatch(loginFailure());
+    }
+	};
+}
+// synchronous function
+// export function loginRequest(email, password) {
+// 	return (dispatch) => {
+// 		boundLogin(email, password);
+
+// 		return fetch('http://localhost:8564/login-success.json')
+// 			.then((res) => res.json())
+// 			.then((json) => dispatch(loginSuccess()))
+// 			.catch((error) => dispatch(loginFailure()));
+// 	};
+// }
